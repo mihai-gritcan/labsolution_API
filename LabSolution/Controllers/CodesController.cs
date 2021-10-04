8014b833dfd5ee3686536bcc4b77277a5dfa8615
Mihai Gritcan <mihail.gritcan@arobs.com>
2021-10-05 00:32:05 +0300

    Add barcode endpoint

--- LabSolution/Controllers/CodesController.cs
+++ LabSolution/Controllers/CodesController.cs
@@ -1,8 +1,11 @@
-﻿using LabSolution.HttpModels;
+﻿using BarcodeLib;
 using LabSolution.Services;
 using LabSolution.Utils;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace LabSolution.Controllers
@@ -20,17 +23,31 @@ namespace LabSolution.Controllers
         [HttpGet("numericCode")]
         public async Task<ActionResult> GetNumericCode([FromQuery] int orderId)
         {
-            var orderScheduleDateTime = await _orderService.GetOrderScheduledDateTime(orderId);
-            return Ok(NumericCodeProvider.GenerateNumericCode(orderScheduleDateTime, orderId));
+            var orderDetails = await _orderService.GetOrderDetails(orderId);
+            if (orderDetails is null) return NotFound();
+
+            return Ok(NumericCodeProvider.GenerateNumericCode(orderDetails.Scheduled, orderId));
         }
 
         [HttpGet("barCode")]
         public async Task<ActionResult> GetBarCode([FromQuery] int orderId)
         {
-            var orderScheduleDateTime = await _orderService.GetOrderScheduledDateTime(orderId);
-            var numericCode = NumericCodeProvider.GenerateNumericCode(orderScheduleDateTime, orderId);
+            var orderDetails = await _orderService.GetOrderDetails(orderId);
+            if (orderDetails is null) return NotFound();
 
-            return Ok();
+            var numericCode = NumericCodeProvider.GenerateNumericCode(orderDetails.Scheduled, orderId);
+
+            var barcode = new Barcode();
+            var img = barcode.Encode(TYPE.CODE39, numericCode.ToString(), Color.Black, Color.White, 250, 100);
+            var data = ConvertImageToBytes(img);
+            return Ok(File(data, "image/jpeg"));
+        }
+
+        private static byte[] ConvertImageToBytes(Image image)
+        {
+            using var ms = new MemoryStream();
+            image.Save(ms, ImageFormat.Png);
+            return ms.ToArray();
         }
     }
 }
