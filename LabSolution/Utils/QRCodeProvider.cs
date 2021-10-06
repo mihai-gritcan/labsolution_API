e052df89fa18f375b2fda9524fb1e280a77b8970
Mihai Gritcan <mihail.gritcan@arobs.com>
2021-10-07 00:11:09 +0300

    Generate QRCode endpoint

---
+++ LabSolution/Utils/QRCodeProvider.cs
@@ -0,0 +1,25 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace LabSolution.Utils
+{
+    public static class QRCodeProvider
+    {
+        public static byte[] GeneratQRCode(long numericCode)
+        {
+            var qrCodeGenerator = new QRCodeGenerator();
+            var qrCodeData = qrCodeGenerator.CreateQrCode(numericCode.ToString(), QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            Bitmap bitmap = qrCode.GetGraphic(15);
+            return ConvertBitmapToBytes(bitmap);
+        }
+
+        private static byte[] ConvertBitmapToBytes(Bitmap bitmap)
+        {
+            var ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            return ms.ToArray();
+        }
+    }
+}
