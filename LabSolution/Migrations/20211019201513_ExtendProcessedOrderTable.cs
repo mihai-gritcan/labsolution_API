6fd4f10b16bf55273ce7bebe41df20e3584bdaa5
Mihai Gritcan <mihail.gritcan@arobs.com>
2021-10-19 23:16:33 +0300

    Extend ProcessedOrder table

---
+++ LabSolution/Migrations/20211019201513_ExtendProcessedOrderTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LabSolution.Migrations
+{
+    public partial class ExtendProcessedOrderTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "NumericCode",
+                table: "ProcessedOrder",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumericCode",
+                table: "ProcessedOrder");
+        }
+    }
+}
