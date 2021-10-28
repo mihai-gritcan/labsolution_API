0c5c2086132f69e564fbfe97dd19749a6e4d220c
Mihai Gritcan <mihail.gritcan@arobs.com>
2021-10-29 02:02:00 +0300

    password complexity details

--- LabSolution/HttpModels/UserRegisterRequest.cs
+++ LabSolution/HttpModels/UserRegisterRequest.cs
@@ -7,6 +7,15 @@ namespace LabSolution.HttpModels
         [Required]
         [StringLength(50)]
         public string Username { get; set; }
+
+        /// <summary>
+        /// At least one upper case English letter, (?=.*?[A-Z])
+        /// At least one lower case English letter, (?=.*?[a - z])
+        /// At least one digit, (?=.*?[0 - 9])
+        /// At least one special character, (?=.*?[#?!@$%^&*-])
+        /// Minimum eight in length.{8,}(with the anchors)
+        /// </summary>
+        /// <see cref="https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a"/>
         [Required]
         [RegularExpression("^(?=.*?[A - Z])(?=.*?[a - z])(?=.*?[0 - 9])(?=.*?[#?!@$%^&*-]).{8,}$)")]
         public string Password { get; set; }
