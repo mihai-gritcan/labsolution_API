92feb211323a38b2b6e06d42db877efff22ed0e3
Mihai Gritcan <mihail.gritcan@arobs.com>
2021-11-18 18:46:09 +0200

    Introduce Serilog

--- LabSolution/Program.cs
+++ LabSolution/Program.cs
@@ -1,23 +1,38 @@
+using LabSolution.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace LabSolution
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                // Make sure that when application crashes, logs don't get lost.
+                Log.CloseAndFlush();
+                Thread.Sleep(SerilogConfiguration.LOG_FLUSH_DELAY_MILISECONDS);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
